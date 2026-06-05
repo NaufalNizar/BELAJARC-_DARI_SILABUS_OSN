6aebd5a445ad781e8f50222d0afe2ca6a5aa888e
Muhammad Naufal Annizar <nizarnoval768@gmail.com>
2026-06-05 20:27:16 +0700

    Add files via upload

---
+++ Tipe_Data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+//Macam-macam tipe data
+//integer adalah tipe data yang menyimpan bilangan bulat contoh 1, 0, -1(tanpa koma)
+    int a = 12;
+
+//float adalah tipe data yang menyimpan bilangan pecahan atau desimal contoh 3.14, 0,5
+    float b = 3.14;
+//double adalah kapasitas yang lebih besar dari float
+    double c =  3.141592653589793;
+//char adalah tipe data yang menyimpan satu huruf tunggal contoh 'D', '#'
+    char d = 'A';
+//Tipe data logika yang hanya memiliki dua kemungkinan nilai: true (benar/1) atau false (salah/0)
+    bool e = true;
+
+    cout<<a<<" "<<b<<" ";
+    cout<<c<<" ";
+    cout<<d<<" "<<e;
+
+}
