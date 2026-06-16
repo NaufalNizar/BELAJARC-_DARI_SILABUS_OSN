c1c3fd9f7eb4847829c8b52aa92897b3caa31c2b
Muhammad Naufal Annizar <nizarnoval768@gmail.com>
2026-06-16 20:26:24 +0700

    Add files via upload

---
+++ Masukan_Keluaran.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //deklarasi variabel
+    int panjang;
+    int lebar;
+    int luas;
+
+    //masukan
+    cin>>panjang;
+    cin>>lebar;
+
+    //penghitungan
+    luas = panjang * lebar;
+
+    //keluaran
+    cout<<luas<<endl;
+
+    return 0;
+}
